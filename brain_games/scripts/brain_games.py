5795f13a9b6fad8fe3f0196c4a26f7e500c99c07
Nikita Laskovykh <lasnick711@gmail.com>
2025-06-16 09:20:51 +0300

    step 4

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,14 @@
 import prompt
-prompt
+
+# import sys
+# print(sys.executable)
+
+
 def main():
     print("Welcome to the Brain Games!")
+    name = prompt.string('May I have your name? ')
+    print(f"Hello, {name}!")
+
 
 if __name__ == '__main__':
     main()
